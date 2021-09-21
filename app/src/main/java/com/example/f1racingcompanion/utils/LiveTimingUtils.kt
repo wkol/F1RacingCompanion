3d836fbf4cd430eb5892e81251bf613d26659934
wojkol <wojtekk61@gmail.com>
2021-09-21 21:43:33 +0200

    Add utilities to decode messages from the websocket

--- app/src/main/java/com/example/f1racingcompanion/utils/LiveTimingUtils.kt
+++ app/src/main/java/com/example/f1racingcompanion/utils/LiveTimingUtils.kt
@@ -1,18 +1,47 @@
 package com.example.f1racingcompanion.utils
 
-import com.example.f1racingcompanion.api.Formula1LiveTimingApi
+import com.google.gson.Gson
+import java.io.ByteArrayOutputStream
+import android.util.*
+
+import java.util.zip.Inflater
 
 object LiveTimingUtils {
-    suspend fun getConnectionToken(api: Formula1LiveTimingApi, hubData: String): String {
-        val response = api.negotiate(hubData)
-        return response.connectionToken
+    fun createHubMessage(method: String, args: List<String>): String {
+        return Gson().toJson(
+            mutableMapOf(
+                'H' to Constants.hubName,
+                'M' to method,
+                'A' to args,
+                'I' to 1
+            )
+        )
     }
-    suspend fun checkForActiveSession(api: Formula1LiveTimingApi): Boolean? {
-        val response = api.streamingStatus()
-        return when(response.status) {
-            "Offline" -> false
-            "Online" -> true
-            else -> null
+
+    fun decodeMessage(text: String): String {
+        val byteArray = Base64.decode(text, Base64.DEFAULT) // I relay that given text is correct b64 encoded
+        return byteArray.zlibDecompress()
+    }
+}
+fun ByteArray.zlibDecompress(): String {
+    val inflater = Inflater(true) // Given data is without any header - only raw data
+
+    val decompressedStream = ByteArrayOutputStream()
+
+    return decompressedStream.use {
+        val buffer = ByteArray(1024)
+
+        inflater.setInput(this)
+
+        // Decompress data in chunks of 1024 bytes until end of the data
+        var count = -1
+        while (count != 0) {
+            count = inflater.inflate(buffer)
+            decompressedStream.write(buffer, 0, count)
         }
+
+        inflater.end()
+
+        decompressedStream.toString(Charsets.UTF_8.name())
     }
 }
