d2467cb9fa31d24c5da062fdd5e163130deeb3c6
wojkol <wojtekk61@gmail.com>
2021-09-23 13:14:43 +0200

    Add websocket url creator

--- app/src/main/java/com/example/f1racingcompanion/utils/LiveTimingUtils.kt
+++ app/src/main/java/com/example/f1racingcompanion/utils/LiveTimingUtils.kt
@@ -3,6 +3,7 @@ package com.example.f1racingcompanion.utils
 import com.google.gson.Gson
 import java.io.ByteArrayOutputStream
 import android.util.*
+import okhttp3.HttpUrl
 
 import java.util.zip.Inflater
 
@@ -10,7 +11,7 @@ object LiveTimingUtils {
     fun createHubMessage(method: String, args: List<String>): String {
         return Gson().toJson(
             mutableMapOf(
-                'H' to Constants.hubName,
+                'H' to Constants.HUB_NAME,
                 'M' to method,
                 'A' to args,
                 'I' to 1
@@ -22,6 +23,22 @@ object LiveTimingUtils {
         val byteArray = Base64.decode(text, Base64.DEFAULT) // I relay that given text is correct b64 encoded
         return byteArray.zlibDecompress()
     }
+
+    fun createWebSocketUrl(token: String, connectionData: String = "[{\"name\": \"Streaming\"}]"): String {
+        val url = HttpUrl.Builder()
+            .scheme("https")
+            .host(Constants.WEBSCOKET_HOST)
+            .addPathSegment(Constants.WEBSOCKET_SIGNALR_PATH)
+            .addPathSegment(Constants.WEBSOCKET_CONNECT_PATH)
+            .addQueryParameter("transport", Constants.WEBSCOKET_TRANSPORT)
+            .addQueryParameter("connectionToken", token)
+            .addQueryParameter("connectionData", connectionData)
+            .addQueryParameter("clientProtocol", Constants.WEBSCOKET_PROTOCOL)
+            .build().toUrl()
+        val res = url.toString().replace("https", "wss")
+        return res
+    }
+
 }
 fun ByteArray.zlibDecompress(): String {
     val inflater = Inflater(true) // Given data is without any header - only raw data
