127273d4eeab74c8d9c82dfe1034ebe3f98c8b40
wojkol <wojtekk61@gmail.com>
2021-09-26 20:53:32 +0200

    Create cookie jar to provide cookies from request to webSocket connection

---
+++ app/src/main/java/com/example/f1racingcompanion/utils/NegotiateCookieJar.kt
@@ -0,0 +1,16 @@
+package com.example.f1racingcompanion.utils
+
+import okhttp3.Cookie
+import okhttp3.CookieJar
+import okhttp3.HttpUrl
+
+class NegotiateCookieJar: CookieJar {
+    private val cookies = mutableListOf<Cookie>()
+
+    override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+        this.cookies.clear()
+        this.cookies.addAll(cookies)
+    }
+
+    override fun loadForRequest(url: HttpUrl): List<Cookie> = cookies
+}
