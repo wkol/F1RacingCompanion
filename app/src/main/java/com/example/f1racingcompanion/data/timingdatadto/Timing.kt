acfedf9bded3b2c6e00a947af196fe3b173f1378
wojkol <wojtekk61@gmail.com>
2021-10-06 14:50:54 +0200

    Fixed missing JsonClass annotation

--- app/src/main/java/com/example/f1racingcompanion/data/timingdatadto/Timing.kt
+++ app/src/main/java/com/example/f1racingcompanion/data/timingdatadto/Timing.kt
@@ -3,6 +3,7 @@ package com.example.f1racingcompanion.data.timingdatadto
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+@JsonClass(generateAdapter = true)
 data class Timing(
     @Json(name = "Sectors")
     val sector: Map<String, SectorValue>?,
