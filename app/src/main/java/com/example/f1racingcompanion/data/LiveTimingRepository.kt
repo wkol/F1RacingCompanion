21c5e2c9467a19030d9498f4bd211df6182226d8
wojkol <wojtekk61@gmail.com>
2021-10-07 15:56:16 +0200

    Add support for TimingData and TimingStats

--- app/src/main/java/com/example/f1racingcompanion/data/LiveTimingRepository.kt
+++ app/src/main/java/com/example/f1racingcompanion/data/LiveTimingRepository.kt
@@ -36,7 +36,8 @@ class LiveTimingRepository (private val webSocketService: LiveTimingService, pri
                 emit(when(line.messageData[0]) {
                     "CarData.z" -> moshi.adapter(CarDataDto::class.java).fromJson(decodeMessage(line.messageData[1]))!!
                     "Position.z" -> moshi.adapter(PositionDataDto::class.java).fromJson(decodeMessage(line.messageData[1]))!!
-                    // Ignore invalid data
+                    "TimingData" -> moshi.adapter(TimingData::class.java).fromJson(line.messageData[1])!!
+                    "TimingStats" -> moshi.adapter(TimingStats::class.java).fromJson(line.messageData[1])!!
                     else -> continue
                 })
             }
