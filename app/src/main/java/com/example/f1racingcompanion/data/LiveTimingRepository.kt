0ee4708b8b8a3da5f56811f94ed05684dbf3c20d
wojkol <wojtekk61@gmail.com>
2021-10-18 21:47:43 +0200

    Add missing LiveTimingRepository methods

--- app/src/main/java/com/example/f1racingcompanion/data/LiveTimingRepository.kt
+++ app/src/main/java/com/example/f1racingcompanion/data/LiveTimingRepository.kt
@@ -1,45 +1,65 @@
 package com.example.f1racingcompanion.data
 
 import com.example.f1racingcompanion.api.LiveTimingService
-import com.example.f1racingcompanion.data.cardatadto.CarDataDto
-import com.example.f1racingcompanion.data.positiondatadto.PositionDataDto
 import com.example.f1racingcompanion.utils.Constants
-import com.example.f1racingcompanion.utils.LiveTimingUtils.decodeMessage
-import com.squareup.moshi.Moshi
 import com.tinder.scarlet.websocket.WebSocketEvent
 import kotlinx.coroutines.flow.*
 import timber.log.Timber
 
-class LiveTimingRepository (private val webSocketService: LiveTimingService, private val moshi: Moshi) {
+class LiveTimingRepository(private val webSocketService: LiveTimingService) {
 
     fun startWebSocket() = flow {
         webSocketService.observeEvents().collect { event ->
-            if(event is WebSocketEvent.OnConnectionOpened) {
+            if (event is WebSocketEvent.OnConnectionOpened) {
                 webSocketService.subscribeToTopics(
                     Subscribe(
-                    "Streaming",
-                    "Subscribe",
-                    listOf(Constants.SUBSRIBED_STREAMS),
-                    0
-                ))
-                Timber.d("Send subscribe request")
+                        "Streaming",
+                        "Subscribe",
+                        listOf(Constants.SUBSRIBED_STREAMS),
+                        0
+                    )
+                )
+                Timber.d("Sent subscribe request")
             }
             emit(event)
             Timber.d("Received event: ${event::class.java.simpleName}")
         }
     }
 
-    fun getData() = flow {
-        webSocketService.observeData().collect { data ->
-            Timber.d("Received data in from of $data")
-            for(line in data) {
-                emit(when(line.messageData[0]) {
-                    "CarData.z" -> moshi.adapter(CarDataDto::class.java).fromJson(decodeMessage(line.messageData[1]))!!
-                    "Position.z" -> moshi.adapter(PositionDataDto::class.java).fromJson(decodeMessage(line.messageData[1]))!!
-                    "TimingData" -> moshi.adapter(TimingDataDto::class.java).fromJson(line.messageData[1])!!
-                    "TimingStats" -> moshi.adapter(TimingStatsDto::class.java).fromJson(line.messageData[1])!!
-                    else -> continue
-                })
+    fun getTimingStats() = flow {
+        webSocketService.observeTimingStats().collect { data ->
+            Timber.d("Received TimingStatsData")
+            data.forEach {
+                emit(it)
+            }
+        }
+    }
+
+    fun getTimingData() = flow {
+        webSocketService.observeTimingData().collect { data ->
+            Timber.d("Received TimingData")
+            data.forEach {
+                emit(it)
+            }
+        }
+    }
+
+    fun getDriverTelemetry(number: Int) = flow {
+        webSocketService.observeTelemetry().collect { data ->
+            Timber.d("Received CarData")
+            data.forEach {
+                for (entry in it.data!!.entries) {
+                    emit(entry[number])
+                }
+            }
+        }
+    }
+
+    fun getPositions() = flow {
+        webSocketService.observeCarPosition().collect { data ->
+            Timber.d("Received PositionData")
+            data.forEach {
+                emit(it)
             }
         }
     }
