056b8c0bf5d17df58da27ea48019c5a05cf49714
wojkol <wojtekk61@gmail.com>
2021-11-01 12:34:58 +0100

    Add missing tests for repository

--- app/src/main/java/com/example/f1racingcompanion/data/LiveTimingRepository.kt
+++ app/src/main/java/com/example/f1racingcompanion/data/LiveTimingRepository.kt
@@ -8,13 +8,16 @@ import com.example.f1racingcompanion.api.LiveTimingService
 import com.example.f1racingcompanion.data.liveTimingData.LiveTimingData
 import com.example.f1racingcompanion.utils.Constants
 import com.tinder.scarlet.websocket.WebSocketEvent
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
 import timber.log.Timber
+import javax.inject.Singleton
 
+@Singleton
 class LiveTimingRepository(private val webSocketService: LiveTimingService) {
 
-    fun startWebSocket() = flow {
-        webSocketService.observeEvents().collect { event ->
+    fun startWebSocket() =
+        webSocketService.observeEvents().onEach { event ->
             if (event is WebSocketEvent.OnConnectionOpened) {
                 webSocketService.subscribeToTopics(
                     Subscribe(
@@ -26,44 +29,30 @@ class LiveTimingRepository(private val webSocketService: LiveTimingService) {
                 )
                 Timber.d("Sent subscribe request")
             }
-            emit(event)
             Timber.d("Received event: ${event::class.java.simpleName}")
-        }
-    }
+    }.flowOn(Dispatchers.IO)
 
-    fun getTimingStats() = flow {
-        webSocketService.observeTimingStats().collect { data ->
+    fun getTimingStats() = webSocketService.observeTimingStats().onEach {
             Timber.d("Received TimingStatsData")
-            emit(data)
-        }
-    }
+    }.flowOn(Dispatchers.IO)
 
-    fun getTimingData() = flow {
-        webSocketService.observeTimingData().collect { data ->
-            emit(data)
-            Timber.d("Received TimingData")
-//            data.forEach {
-//                emit(it)
-//            }
-//        }
-        }
-    }
+    fun getTimingData() =
+        webSocketService.observeTimingData().onEach {
+            Timber.d("Received TimingData") }
+            .flowOn(Dispatchers.IO)
 
     fun getDriverTelemetry(number: Int) = flow {
-        webSocketService.observeTelemetry().collect { data ->
+        webSocketService.observeTelemetry().flowOn(Dispatchers.IO).collect { data ->
             Timber.d("Received CarData")
             for (entry in data.data!!.entries) {
-                emit(entry[number])
+                emit(entry.cars[number])
             }
         }
-    }
+    }.flowOn(Dispatchers.IO)
 
-    fun getPositions() = flow {
-        webSocketService.observeCarPosition().collect { data ->
+    fun getPositions() = webSocketService.observeCarPosition().onEach {
             Timber.d("Received PositionData")
-            emit(data)
-        }
-    }
+    }.flowOn(Dispatchers.IO)
 
 }
 
