dfef9c0d8c3c59a402277160e504118ad538cfd7
wojkol <wojtekk61@gmail.com>
2021-11-01 12:34:45 +0100

    Create HomeViewModel

---
+++ app/src/main/java/com/example/f1racingcompanion/ui/home/HomeViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.f1racingcompanion.ui.home
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.f1racingcompanion.api.LiveTimingService
+import com.example.f1racingcompanion.data.Formula1Repository
+import com.example.f1racingcompanion.utils.Constants
+import com.example.f1racingcompanion.utils.Result
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class HomeViewModel @Inject constructor(private val repository: Formula1Repository): ViewModel() {
+
+    private val _isActive: MutableState<RaceStatusState> = mutableStateOf(RaceStatusState())
+    val isActive: State<RaceStatusState> = _isActive
+
+    private var connectionToken: String? = ""
+
+    init {
+        checkRacingStatus()
+    }
+
+    private fun checkRacingStatus() {
+        repository.checkForActiveSession().onEach {
+            when(it) {
+                is Result.Loading -> _isActive.value = RaceStatusState(null, true)
+                is Result.Success -> _isActive.value = RaceStatusState(it.data, false)
+                is Result.Error -> _isActive.value = RaceStatusState(null, false, it.msg)
+            }
+        }.launchIn(viewModelScope)
+    }
+
+    private fun negotiateConnection() {
+        repository.getConnectionToken().onEach {
+            connectionToken = when(it) {
+                is Result.Loading -> null
+                is Result.Success -> it.data
+                is Result.Error -> null
+            }
+        }.launchIn(viewModelScope)
+    }
+}
