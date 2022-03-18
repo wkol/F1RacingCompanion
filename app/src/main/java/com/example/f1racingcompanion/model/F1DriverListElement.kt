ed2712465457b22f11bcbe8a348a7674b92fc5ba
wojkol <wojtekk61@gmail.com>
2022-03-18 14:34:14 +0100

    Removed redundant F1DriverClass

--- app/src/main/java/com/example/f1racingcompanion/model/F1DriverListElement.kt
+++ app/src/main/java/com/example/f1racingcompanion/model/F1DriverListElement.kt
@@ -3,7 +3,7 @@ package com.example.f1racingcompanion.model
 import com.example.f1racingcompanion.data.timingdatadto.BestLap
 import com.example.f1racingcompanion.data.timingdatadto.SectorValue
 
-class F1DriverListElement(
+data class F1DriverListElement(
     var lastLapTime: String,
     var lastSectors: MutableMap<String, SectorValue>,
     var tires: Tires,
@@ -15,18 +15,57 @@ class F1DriverListElement(
     var retired: Boolean,
     var pitstopCount: Int,
     var startingGridPos: Int,
-    firstName: String,
-    lastName: String,
-    carNumber: Int,
-    shortcut: String,
-    team: String,
-    teamColor: Long
-) : F1Driver(firstName, lastName, carNumber, shortcut, team, teamColor) {
-
+    var firstName: String,
+    var lastName: String,
+    var carNumber: Int,
+    var shortcut: String,
+    var team: String,
+    var teamColor: Long,
+    var isExpanded: Boolean = false
+) {
     fun updateLastSectors(sector: Map<String, SectorValue>) {
+
         if (sector.contains("1")) {
             lastSectors.clear()
         }
         lastSectors.plus(sector)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is F1DriverListElement) return false
+        if (!super.equals(other)) return false
+
+        if (lastLapTime != other.lastLapTime) return false
+        if (lastSectors != other.lastSectors) return false
+        if (tires != other.tires) return false
+        if (position != other.position) return false
+        if (interval != other.interval) return false
+        if (toFirst != other.toFirst) return false
+        if (bestLap != other.bestLap) return false
+        if (inPit != other.inPit) return false
+        if (retired != other.retired) return false
+        if (pitstopCount != other.pitstopCount) return false
+        if (startingGridPos != other.startingGridPos) return false
+        if (isExpanded != other.isExpanded) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + lastLapTime.hashCode()
+        result = 31 * result + lastSectors.hashCode()
+        result = 31 * result + tires.hashCode()
+        result = 31 * result + position
+        result = 31 * result + interval.hashCode()
+        result = 31 * result + toFirst.hashCode()
+        result = 31 * result + bestLap.hashCode()
+        result = 31 * result + inPit.hashCode()
+        result = 31 * result + retired.hashCode()
+        result = 31 * result + pitstopCount
+        result = 31 * result + startingGridPos
+        result = 31 * result + isExpanded.hashCode()
+        return result
     }
 }
